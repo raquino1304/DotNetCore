e4537840d88c23b87dabecf1836285230d91410d
Renato Aquino <renato-ra@hotmail.com>
2017-09-02 21:48:14 -0300

    Primeiro Commit

---
+++ DotNetCore/README.md
@@ -0,0 +1,2 @@
+# DotNetCore
+Repositório para projetos AspNetCore
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Asp.NETCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .Configure(app => {
+                        app.Run(context => context.Response.WriteAsync("Olá Mundo"));
+                    }
+                )
+                .Build();
+                host.Run();
+
+            
+        }
+    }
+}
