f163dd221e556dcbd037b12765bc97b47c8e1a15
Renato Aquino <renato-ra@hotmail.com>
2017-09-04 16:05:18 -0300

    arquivos de settings json

--- Startup.cs
+++ Startup.cs
@@ -5,15 +5,27 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Asp.NETCore
 {
     public class Startup
     {
+        private IConfigurationRoot _configuration;
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+            builder.AddEnvironmentVariables();
+            _configuration = builder.Build();
+        }
         public void Configure(IApplicationBuilder app)
         {
             app.UseMiddleware<MyMiddleware>();
-            app.Run(context => context.Response.WriteAsync("Ola Mundo 2 | "));
+            var applicationName = _configuration.GetValue<string>("ApplicationName");
+            app.Run(context => context.Response.WriteAsync($"Ola Mundo 2 | {applicationName} "));
         }
 
     }
