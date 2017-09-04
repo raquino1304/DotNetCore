b49e0cc00340f7f6ef9d774670836ee0c2033616
Renato Aquino <renato-ra@hotmail.com>
2017-09-04 07:18:26 -0300

    Adicionando os Middleware

---
+++ MyMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Asp.NETCore
+{
+    public class MyMiddleware
+    {
+        private RequestDelegate _next;
+        public MyMiddleware( RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            //Request
+            var start = DateTime.Now;
+            await _next(context);
+
+            //Response
+            var finish = DateTime.Now;
+            var diff = finish.Subtract(start);
+            await context.Response.WriteAsync($"The Time was {diff.Milliseconds}");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace Asp.NETCore
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMiddleware<MyMiddleware>();
+            app.Run(context => context.Response.WriteAsync("Ola Mundo 2 | "));
+        }
+    }
+}
